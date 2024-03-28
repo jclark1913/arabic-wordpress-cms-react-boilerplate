56889af705e894a02af6b786aba8cf95e22b52ac
Justin Clark <jclarksummit@gmail.com>
2024-03-28 17:49:09 -0400

    Add basic utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sitebackground: "rgba(var(--site-background))",
+        primarytext: "rgba(var(--primary-text))",
+        secondarytext: "rgba(var(--secondary-text))",
+        normalborder: "rgba(var(--normal-border))",
+      },
+    },
   },
   plugins: [],
 }
