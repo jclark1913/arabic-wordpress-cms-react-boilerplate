9f4798391a88172349d150217d447966ce507143
Justin Clark <jclarksummit@gmail.com>
2024-03-29 13:25:36 -0400

    Add routing with StaticPage component

--- src/routes/RoutesList.tsx
+++ src/routes/RoutesList.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { Navigate } from "react-router-dom";
-import About from '../components/About';
+import StaticPage from '../components/StaticPage';
 
 const RoutesList: React.FC = () => {
     return (
         <>
             <Routes>
-                {/* <Route path="/" element={<Homepage />} /> */}
-                <Route path="/about" element={<About />} />
-                {/* <Route path="/contact" element={<Contact />} /> */}
+                {/* <Route path="/" element={<StaticPage slug="home" />} /> */}
+                <Route path="/about" element={<StaticPage slug="about" />} />
+                <Route path="/contact" element={<StaticPage slug="contact" />} />
                 {/* <Route path="*" element={<Navigate to="/" />} /> */}
             </Routes>
         </>
