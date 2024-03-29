c538313ce1c052c9e7bd7b980e5b9da1b58d4f6e
Justin Clark <jclarksummit@gmail.com>
2024-03-29 13:25:06 -0400

    Add ToC/change order

--- src/constants/titles.ts
+++ src/constants/titles.ts
@@ -3,11 +3,15 @@ export const SITE_TITLE = "عنوان الموقع";
 export const NAV_LINKS = [
   {
     "title": "الصفحة الرئيسية",
-    "path": "/home"
+    "path": "/"
   },
   {
     "title": "عن الموقع",
     "path": "/about"
+  },
+  {
+    "title": "جدول المحتويات",
+    "path": "/table-of-contents"
   },
   {
     "title": "اتصل بنا",
