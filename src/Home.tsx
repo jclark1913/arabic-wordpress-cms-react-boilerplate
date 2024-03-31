2bfff1594a7c853baa0a75373558561ce1d18e1a
Justin Clark <jclarksummit@gmail.com>
2024-03-31 14:25:00 -0400

    Add stub homepage

---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <section>
+        <h1>عنوان الموقع</h1>
+        <p className="text-lg">هذا الموقع على قيد الانشاء...</p>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
