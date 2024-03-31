4419e166635f7afbc3b35be7fbb72d4dc19e5cde
Justin Clark <jclarksummit@gmail.com>
2024-03-31 12:56:23 -0400

    Add ToC route

--- src/routes/RoutesList.tsx
+++ src/routes/RoutesList.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { Navigate } from "react-router-dom";
 import StaticPage from '../components/StaticPage';
+import TableOfContents from '../components/TableOfContents';
 
 const RoutesList: React.FC = () => {
     return (
@@ -9,6 +10,7 @@ const RoutesList: React.FC = () => {
                 {/* <Route path="/" element={<StaticPage slug="home" />} /> */}
                 <Route path="/about" element={<StaticPage slug="about" />} />
                 <Route path="/contact" element={<StaticPage slug="contact" />} />
+                <Route path="/table-of-contents" element={<TableOfContents />} />
                 {/* <Route path="*" element={<Navigate to="/" />} /> */}
             </Routes>
         </>
