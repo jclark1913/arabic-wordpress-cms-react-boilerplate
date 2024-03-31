f9ce50f2cc16c5c23cad28122ad96ff98643b855
Justin Clark <jclarksummit@gmail.com>
2024-03-31 14:25:09 -0400

    Add footer + update styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import NavBar from "./components/NavBar";
 import { ThemeProvider } from "./ThemeContext";
 import RoutesList from "./routes/RoutesList";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -8,9 +9,10 @@ function App() {
       <ThemeProvider>
         <main className="flex min-h-screen flex-col bg-sitebackground text-primarytext">
           <NavBar />
-          <div className="container mx-auto">
+          <div className="container mx-auto px-12 p-4 flex flex-col flex-grow">
             <RoutesList />
           </div>
+        <Footer />
         </main>
       </ThemeProvider>
     </>
