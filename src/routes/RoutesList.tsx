71a7d2e90818febbf743f3574eb3f77619c90189
Justin Clark <jclarksummit@gmail.com>
2024-03-31 14:26:05 -0400

    Update routes to populate posts with StaticPage component

--- src/routes/RoutesList.tsx
+++ src/routes/RoutesList.tsx
@@ -1,20 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
-import StaticPage from '../components/StaticPage';
-import TableOfContents from '../components/TableOfContents';
+import StaticPage from "../components/StaticPage";
+import TableOfContents from "../components/TableOfContents";
+import Post from "../components/Post";
+import Home from "../Home";
 
 const RoutesList: React.FC = () => {
-    return (
-        <>
-            <Routes>
-                {/* <Route path="/" element={<StaticPage slug="home" />} /> */}
-                <Route path="/about" element={<StaticPage slug="about" />} />
-                <Route path="/contact" element={<StaticPage slug="contact" />} />
-                <Route path="/table-of-contents" element={<TableOfContents />} />
-                {/* <Route path="*" element={<Navigate to="/" />} /> */}
-            </Routes>
-        </>
-    )
-}
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<StaticPage slug="about" />} />
+        <Route path="/contact" element={<StaticPage slug="contact" />} />
+        <Route path="/table-of-contents" element={<TableOfContents />} />
+        <Route path="/posts/:slug" element={<StaticPage slug={null} />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
+  );
+};
 
 export default RoutesList;
