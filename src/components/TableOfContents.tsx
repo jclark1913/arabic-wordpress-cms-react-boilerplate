305cc43e0e25a7df78e8d30d39cdd8cf3685b2c0
Justin Clark <jclarksummit@gmail.com>
2024-03-31 12:55:54 -0400

    Add reducer to filter categories + category component

--- src/components/TableOfContents.tsx
+++ src/components/TableOfContents.tsx
@@ -1,28 +1,42 @@
 import { useState, useEffect } from "react";
 import { WordPressAPI } from "../api/api";
-
-interface Category {
-  id: number;
-  name: string;
-  slug: string;
-}
+import { CategoryInterface } from "./Category";
+import Category from "./Category";
 
 const TableOfContents: React.FC = () => {
-  const [categories, setCategories] = useState<Category[]>([]);
+  const [categories, setCategories] = useState<CategoryInterface[]>([]);
 
   useEffect(() => {
     const getCategoriesOnMount = async () => {
       const response = await WordPressAPI.getCategories();
-      const categories = response.categories.map((category: any) => ({
-        id: category.ID,
-        name: category.name,
-        slug: category.slug,
-      }));
+      const categories = response.categories.reduce(
+        (acc: CategoryInterface[], category: any) => {
+          if (category.post_count > 0) {
+            acc.push({
+              id: category.ID,
+              name: category.name,
+              slug: category.slug,
+            });
+          }
+          return acc;
+        },
+        []
+      );
       setCategories(categories);
     };
-  });
+    getCategoriesOnMount();
+  }, []);
 
-  return <></>;
+  return (
+    <div className="">
+      <h2 className="text-3xl font-bold text-center mb-4">جدول المحتويات</h2>
+      <div>
+        {categories.map((category, index) => (
+          <Category key={index} category={category} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TableOfContents;
